5be749edf3d408a18fb2022695e2e48a1acf1878
Emanuel Ditzel <emanuel.ditzel@nybblegroup.com>
2022-11-08 15:42:03 -0300

    Adding 'LoginTestCase' class

--- src/test/java/todolyPages/LoginModal.java
+++ src/test/java/todolyPages/LoginModal.java
@@ -1,6 +1,7 @@
 package todolyPages;
 
 import controlSelenium.Button;
+import controlSelenium.Label;
 import controlSelenium.TextBox;
 import org.openqa.selenium.By;
 
@@ -9,6 +10,9 @@ public class LoginModal {
     public TextBox password = new TextBox(By.id("ctl00_MainContent_LoginControl1_TextBoxPassword"),"[Password] textBox in Login modal");
     public Button resetPasswordButton = new Button(By.xpath("//a[@class='HPLoginForgotLink']"),"[Reset your Password] button in Login modal");
     public Button loginButton = new Button(By.id("ctl00_MainContent_LoginControl1_ButtonLogin"),"[Login] button in Login modal");
+    public TextBox resetPwdEmailField = new TextBox(By.id("ctl00_MainContent_LoginControl1_TextBoxForgotPwdEmail"),"[Reset Password] textBox in Login modal");
+    public Button sendButton = new Button(By.id("ctl00_MainContent_LoginControl1_ButtonSend"),"[Send] button for reset password in Login modal");
+    public Label resetPwdValidationMessage = new Label(By.xpath("//span[text()='An email has been sent with a link where you can change your password.']"), "Mensaje de validación informando que se mandó un link al email ingresado para cambiar contraseña");
 
     public void login(String userEmail, String userPassword){
         email.setText(userEmail);
--- src/test/java/todolyPages/MainPage.java
+++ src/test/java/todolyPages/MainPage.java
@@ -1,8 +1,13 @@
 package todolyPages;
 
+import controlSelenium.Button;
 import controlSelenium.Label;
 import org.openqa.selenium.By;
 
 public class MainPage {
-    public Label mainPageLabel = new Label(By.xpath("//a[normalize-space()='']//img"),"[Label] in main page");
+     public Label mainPageLabel = new Label(By.xpath("//a[normalize-space()='']//img"),"[Label] in main page");
+     public Button inboxButton = new Button(By.xpath("//td[text()='Inbox']"),"[Inbox] button in main page");
+
+    //public Label mainPageLabel = new Label(By.xpath("//img[@src='/Images/logo2.png']"),"[Label] in main page");
+
 }
